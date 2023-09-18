3ad6015ea152ba525006307631beb8c99e0af53a
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-09-18 14:46:52 +0200

    Barrel file

---
+++ src/js/api/listings/index.js
@@ -0,0 +1,5 @@
+export * from '../listings/search.js';
+export * from '../listings/create.js';
+export * from '../listings/read.js';
+// export * from "../posts/update.js";
+// export * from "../posts/delete.js";
