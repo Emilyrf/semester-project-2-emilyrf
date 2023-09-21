ec6a55a3420dd69f4316841740c094e7db845603
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-09-21 16:59:55 +0200

    Bid to another user’s Listing

--- src/js/api/listings/create.js
+++ src/js/api/listings/create.js
@@ -5,12 +5,20 @@ import displayMessage from '../../ui/components/displayMessage.js';
 const action = '/listings';
 const method = 'post';
 
-// Function to create a new listing with proper data
+// Function to check if a string is a valid URL
+function isValidURL(string) {
+  try {
+    new URL(string);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+//Function to create a new listing
 export async function createListing(listingData) {
-  // Generate an ISO 8601 date string for endsAt
   listingData.endsAt = new Date(listingData.endsAt).toISOString();
 
-  // Check if media is an array and contains valid URLs
   if (!Array.isArray(listingData.media)) {
     listingData.media = [];
   } else {
@@ -36,85 +44,42 @@ export async function createListing(listingData) {
       throw new Error(errorResponse.errors[0].message);
     }
   } catch (error) {
-    console.error('Error creating listing:', error);
     displayMessage('danger', error, '#message');
   }
 }
 
-// Function to check if a string is a valid URL
-function isValidURL(string) {
+//Function to create a bid
+export async function createBid(bid) {
   try {
-    new URL(string);
-    return true;
+    const queryString = document.location.search;
+    const params = new URLSearchParams(queryString);
+    let id = params.get('id');
+
+    // Construct the URL for creating a bid
+    const createBidUrl = `${API_AUCTION_URL}${action}/${id}/bids`;
+    if (!id) {
+      throw new Error('Bid requires a listing id');
+    }
+
+    const response = await authFetch(createBidUrl, {
+      method,
+      headers: {
+        'Content-Type': 'application/json', // Set the content type for JSON
+      },
+      body: JSON.stringify(bid),
+    });
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      // Handle the error response
+      // const errorData = await response.json();
+      // throw new Error(errorData.message || "Failed to create bid");
+      const errorResponse = await response.json();
+      const errorMessage = errorResponse.errors[0].message;
+      throw new Error(errorMessage);
+    }
   } catch (error) {
-    return false;
+    displayMessage('danger', error, '#message');
   }
 }
-
-// import { API_AUCTION_URL } from "../constants.js";
-// import { authFetch } from "../authFetch.js";
-
-// const action = "/listings";
-// const method = "post";
-
-// export async function createListing(listingData) {
-//     const createListingUrl = API_AUCTION_URL + action;
-
-//     const response = await authFetch(createListingUrl, {
-//         method,
-//         body: JSON.stringify(listingData)
-//     })
-//     const listing = await response.json();
-
-//     if (response.ok) {
-//         console.log(response);
-
-//     }
-//     // throw new Error(json.errors[0].message);
-//     console.log("error");
-// }
-
-// export async function createListing(listingData) {
-//     try {
-//         const createListingUrl = API_AUCTION_URL + action;
-
-//         const endsAtDate = new Date(listingData.endsAt);
-//         const endsAtISOString = endsAtDate.toISOString();
-//         listingData.endsAt = endsAtISOString;
-
-//         const response = await authFetch(createListingUrl, {
-//             method,
-//             body: JSON.stringify(listingData)
-//         });
-
-//         const listing = await response.json();
-//         console.log(response);
-
-//         if (response.ok) {
-
-//             console.log(response);
-//         }
-
-//     } catch (error) {
-
-//         console.log(error);
-//     }
-// }
-
-// export async function createComment(commentData) {
-//     const queryString = document.location.search;
-//     const params = new URLSearchParams(queryString);
-//     let id = params.get("id");
-//     const createCommentUrl = `${API_AUCTION_URL}${action}/${id}/comment`;
-
-//     const response = await authFetch(createCommentUrl, {
-//         method,
-//         body: JSON.stringify(commentData)
-//     })
-//     const comment = await response.json();
-//     if (response.ok) {
-//         window.location.reload();
-
-//     }
-//     throw new Error(json.errors[0].message);
-// }
