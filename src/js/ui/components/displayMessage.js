c3e26a0e733899c8b0961eecaeacc65a62fa1cfa
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-09-21 18:27:09 +0200

    Improve error and success alerts

--- src/js/ui/components/displayMessage.js
+++ src/js/ui/components/displayMessage.js
@@ -1,10 +1,30 @@
-export default function displayMessage(type, message, target) {
-	const container = document.querySelector(target);
+export default function displayMessage(
+  type,
+  message,
+  target,
+  showRefreshLink = false,
+) {
+  const container = document.querySelector(target);
 
-	container.innerHTML = `
-    <div class="alert alert-${type}">
-      ${message}
-    </div>
-  `;
+  const messageElement = document.createElement('div');
+  messageElement.classList.add('alert', `alert-${type}`);
+
+  if (showRefreshLink) {
+    messageElement.innerHTML = `
+      ${message} <a href="#" id="refreshLink">Refresh</a>
+    `;
+
+    const refreshLink = messageElement.querySelector('#refreshLink');
+    if (refreshLink) {
+      refreshLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        location.reload();
+      });
+    }
+  } else {
+    messageElement.textContent = message;
+  }
+
+  container.innerHTML = '';
+  container.appendChild(messageElement);
 }
-
