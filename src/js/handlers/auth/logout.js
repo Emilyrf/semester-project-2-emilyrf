507495eb055a4c63adb78ccee9f97e40e5b5bc4c
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-09-21 16:52:35 +0200

    Dynamic menu

--- src/js/handlers/auth/logout.js
+++ src/js/handlers/auth/logout.js
@@ -1,8 +1,7 @@
-import * as storage from '../../storage/index.js';
+import * as storage from '../../helpers/storage.js';
 
 export function logoutListener() {
   const logoutButton = document.querySelector('#logout');
-
   logoutButton.addEventListener('click', () => {
     storage.remove('token');
     storage.remove('profile');
