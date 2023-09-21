bc84c0b40a6c50b54b6a6ce06f670e33f7ae5e49
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-09-21 16:49:51 +0200

    Dynamic menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,51 @@
 import * as listeners from './handlers/index.js';
-
 import hideSearchOnDocumentClick from './handlers/listings/hideSearchOnDocumentClick.js';
 import searchListings from './handlers/listings/searchListing.js';
+import { createMenu } from './ui/common/createMenu.js';
 
-const path = location.pathname;
+function router() {
+  const path = location.pathname;
+  createMenu(path);
 
-switch (path) {
-  case '/':
-  case '/index.html':
-    console.log('home');
-    listeners.displayListings();
-    searchListings();
-    hideSearchOnDocumentClick();
-    break;
-  case '/profile/login/':
-    listeners.setLoginFormListener();
-    break;
-  case '/profile/register/':
-    listeners.setRegisterFormListener();
-    break;
-  case '/profile/edit/':
-    console.log('update profile');
-    listeners.setUpdateProfileFormListener();
-    break;
-  case '/profile/':
-    listeners.displayProfileListener();
-    listeners.displayMyListings();
-    break;
-  // case '/post/edit/':
-  //   listeners.setUpdatePostFormListener();
-  //   break;
-  // case '/posts/':
-  //   listeners.setCreatePostFormListener();
-  //   listeners.getPosts();
-  //   listeners.filterListener();
-  //   listeners.searchListener();
-  //   break;
-  case '/listing/':
-    listeners.getListingtById();
-    break;
-  case '/listing/create/':
-    console.log('new listing');
-    listeners.setCreateListingFormListener();
+  switch (path) {
+    case '/':
+    case '/index.html':
+      if (listeners.isLoggedIn()) {
+        document.querySelector('#hero').style.display = 'none';
+        listeners.displayUserName();
+      } else {
+        document.querySelector('#welcomeUser').style.display = 'none';
+      }
+
+      listeners.displayListings();
+      searchListings();
+      hideSearchOnDocumentClick();
+
+      break;
+    case '/profile/login/':
+      listeners.setLoginFormListener();
+      break;
+    case '/profile/register/':
+      listeners.setRegisterFormListener();
+      break;
+    case '/profile/edit/':
+      listeners.setUpdateProfileFormListener();
+      break;
+    case '/profile/':
+      listeners.displayProfileListener();
+      listeners.displayMyListings();
+      break;
+    case '/listing/':
+      if (!listeners.isLoggedIn()) {
+        location.href = '/profile/login/';
+        return false;
+      }
+      listeners.getListingtById();
+      listeners.setCreateBidListener();
+      break;
+    case '/listing/create/':
+      console.log('new listing');
+      listeners.setCreateListingFormListener();
+  }
 }
-
-listeners.logoutListener();
+router();
